6b463a059c003042e975a2a2454edc716bac9017
Wazaabi <wazaabi@gmail.com>
2013-10-29 23:44:42 +0200

    tunnit66

---
+++ pooleli.c
@@ -0,0 +1,15 @@
+#int main(int argc, char * argv[]) {
+
+    double x0=3.0;
+    double x1=11.0;
+    int n=5;
+    int i;
+    double a;
+    double b;
+
+    for(i=0 ; i<n ; i++){
+        printf("i=%d a=%5f b=%f\n", i, a, b);
+    }
+
+    return 0;
+}
