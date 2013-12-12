f4db9b360b640235e4c0d8163e6edb30f7e44f6c
Peeter Rätsepp <p.ratsepp@gmail.com>
2013-12-12 10:59:07 +0200

    Update cnc.c

--- cnc.c
+++ cnc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float koodid (float x, float y, float w, float h){
+void koodid (float x, float y, float w, float h){
 	printf("G X GoTo %f\n", x );
 	printf("G Y GoTo %f\n", y );
 	printf("G Z Down\n");
@@ -9,8 +9,6 @@ float koodid (float x, float y, float w, float h){
 	printf("G X Move %f\n", -w);
 	printf("G Y Move %f\n", -h);
 	printf("G Z Up\n");
-	
-	return 0;
 }
 
 int main(int argc, char * argv[]) {
